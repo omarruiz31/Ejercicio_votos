2ea126beeb05e02a33b9066057c696a4fb0f4d94
Omar Ruiz <omar.ruiz0531@gmail.com>
2026-02-12 21:27:42 -0600

    First_Commit

---
+++ src/Voto.java
@@ -0,0 +1,6 @@
+//Se requiere calcular el porcentaje de votos que están en una urna.
+// Cada que se saca un boto de la urna, se ingresa el número del candidato.
+// Cuando se terminan los votos se ingresa un cero
+public class Voto {
+
+}
